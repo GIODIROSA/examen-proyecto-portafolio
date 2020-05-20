713b642fd14082e4e946053dcaf703ec5fb89792
Giovanni <giodirosaartes@gmail.com>
2020-05-20 02:51:12 -0400

    cuarto commit js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+$(function(){
+    $("h2").click(function(){    
+        $(this).css("color", "#1e56a0" );  });
+        $("a").click(function(){    
+            $(this).attr("target", "_blank" );  });
 
+
+
+
+
+
+
+
+});
